17a6e53dc95d925bdddfbcaccdbbc599c3df5d9f
snehasiskar <snehasiskar2@gmail.com>
2026-09-02 10:45:03 +0000

    Complete authentication milestone

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
+from app.routers.auth import router as auth_router
 app=FastAPI(title="Creator Analytics Dashboard API",version="1.0.0")
+app.include_router(auth_router)
 @app.get("/")
 def root():
     return{
--- backend/app/routers/auth.py
+++ backend/app/routers/auth.py
@@ -1,9 +1,8 @@
-from fastapi import APIRouter,
-HTTPException
+from fastapi import APIRouter,HTTPException
 from pydantic import BaseModel
 from passlib.context import CryptContext
 router=APIRouter(prefix="/auth",tags=["Authentication"])
-pwd_context=CryptContext(schemes=["bcrypt"],deprecated="auto")
+pwd_context=CryptContext(schemes=["pbkdf2_sha256"],deprecated="auto")
 users={}
 class UserCreate(BaseModel):
     username: str
@@ -15,15 +14,35 @@ class UserLogin(BaseModel):
 @router.post("/register")
 def register(user: UserCreate):
     if user.username in users:
-        raise
-        HTTPException(status_code=400,detail="User already exists")
-        hashed_password=pwd_context.hash(user.password)
-        users[user.username]={"password": hashed_password,"role":user.role}
-        @router.post("/login")
-        def login(User:UserLogin):
-            if user.username not in users:
-                raise
-   HTTPException(status_code=401,detail="Invalid username or password")
-   return{
-    "message": "Login successful","username":user.username,"role":stored_user["role"] 
+        raise HTTPException(
+            status_code=400,
+            detail="User already exists"
+        )
+    hashed_password=pwd_context.hash(user.password)
+    users[user.username]={
+        "password": hashed_password,
+        "role":user.role
+    }
+    return {
+        "message": "Registration successful",
+        "username":user.username,
+        "role":user.role
+    }
+@router.post("/login")
+def login(user: UserLogin):
+    if user.username not in users:
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid username or password")
+    stored_user = users[user.username]
+    if not pwd_context.verify(user.password,
+    stored_user["password"]):
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid username or password"
+        )    
+    return{
+        "message": "Login successful",
+        "username":user.username,
+        "role": stored_user["role"]
    }                
